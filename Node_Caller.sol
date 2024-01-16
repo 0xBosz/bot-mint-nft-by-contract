3bfd0d38b6bf8291f4007047140ea428f0fa76e0
0xBosz <32166503+0xBosz@users.noreply.github.com>
2024-01-16 22:47:28 +0700

    add comment

--- Node_Caller.sol
+++ Node_Caller.sol
@@ -5,6 +5,7 @@ contract Node_Caller {
     fallback() external payable {
         assembly {
             calldatacopy(0x0, 0x0, calldatasize())
+            # This address is Host_Minter.sol address
             let success := delegatecall(gas(), 0xB25f1f0B4653b4e104f7Fbd64Ff183e23CdBa582, 0x0, calldatasize(), 0x0, 0)
             if iszero(success) {
                 revert(0x0, returndatasize())
